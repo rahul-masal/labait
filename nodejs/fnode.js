9cbde33df8b15883b8a1ff86a35c32a31092edbe
Mr.Robot <61948407+rahul-masal@users.noreply.github.com>
2022-06-06 12:30:46 +0530

    Add files via upload

---
+++ nodejs/fnode.js
@@ -0,0 +1,8 @@
+var calculator = require('./moduleCal.js')
+
+var a=10,b=5;
+
+console.log("Additions: "+calculator.add(a,b))
+console.log("Substraction: "+calculator.sub(a,b))
+console.log("Multiplication: "+calculator.mul(a,b))
+console.log("Division: "+calculator.div(a,b))
---
+++ nodejs/moduleCal.js
@@ -0,0 +1,16 @@
+//Returns addition of two numbers
+
+exports.add=function(a,b){
+    return a+b
+}
+
+exports.sub=function(a,b){
+    return a-b
+}
+exports.mul=function(a,b){
+    return a*b
+}
+
+exports.div=function(a,b){
+    return a/b
+}
